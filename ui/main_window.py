ce100ecd304f0c588ecec9d6a375a6fb7168f9cf
Daniel Bridges <bridges2@gmail.com>
2013-11-13 06:35:49 -0800

    Better theme.

--- ui/main_window.py
+++ ui/main_window.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file './ui/MainWindow.ui'
 #
-# Created: Tue Nov 12 20:57:34 2013
+# Created: Wed Nov 13 06:34:48 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.2.0
 #
 # WARNING! All changes made in this file will be lost!
